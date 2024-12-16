1bcb89b68fb3db154fdce656dc4ebbbf52ab8e1f
Oluwabi Shefiu <oluwabishefiu@Oluwabis-MBP.lan>
2024-12-16 19:38:24 +0000

    chnaged script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
+# Prevent Homebrew cleanup during installation
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
-brew install cocoapods
-# have to add node yourself
-brew install node@16
-# link it to the path
-brew link node@16
 
-brew install yarn
-# make it excutable
-# Install dependencies you manage with CocoaPods.
-yarn
-# fixes boolean issue 
-# sed -i '' '2232s/|/||/g' /Volumes/workspace/repository/node_modules/react-native/ReactCommon/yoga/yoga/Yoga.cpp
-pod install
-# remove accessibilyi
-# /Users/tek/Documents/Projects/ReactNative/NativeProjects/EaziInvest/ios/Pods/Pods.xcodeproj/xcuserdata/tek.xcuserdatad
-# rm -r /Volumes/workspace/repository/ios/Podszoj/xcuserdata/tek.xcuserdatad/React-Core-AccessibilityResources
-# the sed command from RN cant find the file... so we have to run it ourselves
-# sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
+# Install necessary tools
+brew install cocoapods node@20 yarn
+brew link node@20
+export PATH="/usr/local/opt/node@20/bin:$PATH"
+
+# Install project dependencies
+yarn || { echo "Yarn install failed. Exiting..."; exit 1; }
+pod install || { echo "Pod install failed. Exiting..."; exit 1; }
+
+# Fix React Native Yoga Boolean Issue
+RN_YOGA_PATH=$(find ./node_modules/react-native/ReactCommon/yoga -name "Yoga.cpp" | head -n 1)
+if [ -f "$RN_YOGA_PATH" ]; then
+  sed -i '' '2232s/|/||/g' "$RN_YOGA_PATH" || echo "Failed to apply sed fix for Yoga.cpp."
+else
+  echo "Yoga.cpp not found. Skipping sed command."
+fi
+
+echo "Setup complete!"
