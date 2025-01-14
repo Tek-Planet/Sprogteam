c566cde8c1050cde4734d04452494069263bb6d7
Oluwabi Shefiu <oluwabishefiu@Oluwabis-MBP.lan>
2025-01-14 22:17:11 +0000

    PFP

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -15,21 +15,14 @@ npm install --legacy-peer-deps || { echo "NPM install failed. Exiting..."; exit 
 BOOST_PODSPEC_PATH="./node_modules/react-native/third-party-podspecs/boost.podspec"
 if [ -f "$BOOST_PODSPEC_PATH" ]; then
   echo "Patching Boost podspec to fix checksum issue..."
-  sed -i '' 's|https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.bz2|https://sourceforge.net/projects/boost/files/boost/1.76.0/boost_1_76_0.tar.bz2|' "$BOOST_PODSPEC_PATH" || { echo "Failed to patch Boost podspec. Exiting..."; exit 1; }
-  sed -i '' 's|f0397ba6e982c4450f27bf32a2a83292aba035b827a5623a14636ea583318c41|79e6d3f986444e5a80afbeccdaf2d1c1cf964baa8d766d20859d653a16c39848|' "$BOOST_PODSPEC_PATH" || { echo "Failed to patch Boost podspec checksum. Exiting..."; exit 1; }
+  sed -i '' 's|https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.bz2|https://sourceforge.net/projects/boost/files/boost/1.76.0/boost_1_76_0.tar.bz2|' "$BOOST_PODSPEC_PATH" || { echo "Failed to patch Boost URL. Exiting..."; exit 1; }
+  sed -i '' 's|f0397ba6e982c4450f27bf32a2a83292aba035b827a5623a14636ea583318c41|79e6d3f986444e5a80afbeccdaf2d1c1cf964baa8d766d20859d653a16c39848|' "$BOOST_PODSPEC_PATH" || { echo "Failed to patch Boost checksum. Exiting..."; exit 1; }
 else
   echo "Boost podspec not found. Skipping patch."
 fi
 
-# Apply Yoga boolean issue fix
-RN_YOGA_PATH=$(find ./node_modules/react-native/ReactCommon/yoga -name "Yoga.cpp" | head -n 1)
-if [ -f "$RN_YOGA_PATH" ]; then
-  sed -i '' '2232s/|/||/g' "$RN_YOGA_PATH" || echo "Failed to apply Yoga.cpp fix."
-else
-  echo "Yoga.cpp not found. Skipping Yoga fix."
-fi
-
 # Run CocoaPods installation
+rm -rf Pods Podfile.lock
 pod install || { echo "Pod install failed. Exiting..."; exit 1; }
 
 # Final confirmation message
